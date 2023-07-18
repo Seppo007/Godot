5243b3adbf8c8a30da9df1f635bb8e9db0e5342f
Sebastian Schalow <schalowseb@gmail.com>
2023-07-19 00:28:41 +0200

    Finish elementary course training for godot fundamentals

--- Courses/IntroToGodot/Mover.gd
+++ Courses/IntroToGodot/Mover.gd
@@ -1,9 +1,14 @@
-extends Node2D
+extends Sprite2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	var vec = Vector2(500, 200)
+	global_position = vec
+
+var timer: float = 0.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var direction = Vector2(1,1) * 30 * delta
+	global_position += direction
