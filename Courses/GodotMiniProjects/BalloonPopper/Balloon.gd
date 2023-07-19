3013366175958b70cdc0dad8aaa849042c9f88c0
Sebastian Schalow <schalowseb@gmail.com>
2023-07-19 18:04:50 +0200

    Finish Balloon minigame

--- Courses/GodotMiniProjects/BalloonPopper/Balloon.gd
+++ Courses/GodotMiniProjects/BalloonPopper/Balloon.gd
@@ -1,9 +1,14 @@
 extends Area3D
 
-const CLICKS_TO_POP: int = 3
-const SIZE_INCREASE: float = 0.2
-const SCORE_TO_GIVE: int = 1
+@export var clicks_to_pop: int = 5
+@export var SIZE_INCREASE: float = 0.2
+@export var SCORE_TO_GIVE: int = 1
 
 func _on_input_event(camera, event, position, normal, shape_idx):
 	if(event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed):
-		print("Increased size")
+		scale += Vector3.ONE * SIZE_INCREASE
+		clicks_to_pop -= 1
+		
+		if(clicks_to_pop == 0):
+			get_node("/root/Main").increase_score(SCORE_TO_GIVE)
+			queue_free()
