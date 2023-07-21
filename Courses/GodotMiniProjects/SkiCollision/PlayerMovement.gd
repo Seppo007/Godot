5fd251b62fe0e3eba2476c7d6a56c3084ee75b5a
Sebastian Schalow <schalowseb@gmail.com>
2023-07-22 01:06:19 +0200

    Add goal but still not triggered

--- Courses/GodotMiniProjects/SkiCollision/PlayerMovement.gd
+++ Courses/GodotMiniProjects/SkiCollision/PlayerMovement.gd
@@ -5,5 +5,11 @@ extends RigidBody3D
 func _physics_process(delta):
 	if(Input.is_key_pressed(KEY_LEFT)):
 		linear_velocity.x -= move_speed
-	if(Input.is_key_pressed(KEY_RIGHT)):		
+	if(Input.is_key_pressed(KEY_RIGHT)):
 		linear_velocity.x += move_speed
+
+
+func _on_body_entered(body):
+	if(body.is_in_group("Tree")):
+		get_tree().reload_current_scene()
+		
