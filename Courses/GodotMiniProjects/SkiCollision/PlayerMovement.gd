ce72868e79b5c06f6de5872b43358ed65162d33a
Sebastian Schalow <schalowseb@gmail.com>
2023-07-21 23:32:49 +0200

    Adjust camera and player positioning, add script for player movement

---
+++ Courses/GodotMiniProjects/SkiCollision/PlayerMovement.gd
@@ -0,0 +1,9 @@
+extends RigidBody3D
+
+@export var move_speed: float = 0.1
+
+func _physics_process(delta):
+	if(Input.is_key_pressed(KEY_LEFT)):
+		linear_velocity.x -= move_speed
+	if(Input.is_key_pressed(KEY_RIGHT)):		
+		linear_velocity.x += move_speed
