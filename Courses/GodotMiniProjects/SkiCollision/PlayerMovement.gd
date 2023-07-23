5a34d5a4bc69ab40ffc2731edcc12618fcc5026b
Sebastian Schalow <schalowseb@gmail.com>
2023-07-23 19:26:19 +0200

    Add script for reaching finish. Add safezone below to rest the game when touched

--- Courses/GodotMiniProjects/SkiCollision/PlayerMovement.gd
+++ Courses/GodotMiniProjects/SkiCollision/PlayerMovement.gd
@@ -1,15 +1,28 @@
 extends RigidBody3D
 
 @export var move_speed: float = 0.1
+var finished: bool = false
 
 func _physics_process(delta):
-	if(Input.is_key_pressed(KEY_LEFT)):
-		linear_velocity.x -= move_speed
-	if(Input.is_key_pressed(KEY_RIGHT)):
-		linear_velocity.x += move_speed
-
+	if(!finished):
+		if(Input.is_key_pressed(KEY_LEFT)):
+			linear_velocity.x -= move_speed
+		if(Input.is_key_pressed(KEY_RIGHT)):
+			linear_velocity.x += move_speed
 
 func _on_body_entered(body):
 	if(body.is_in_group("Tree")):
 		get_tree().reload_current_scene()
-		
+
+func _on_goal_body_entered(body: RigidBody3D):
+	if(body.is_in_group("Player")):
+		finished = true
+		body.linear_velocity *= 0.15
+		var material = PhysicsMaterial.new()
+		material.friction = 1.0
+		body.physics_material_override = material
+
+
+func _on_out_of_game_area_body_entered(body):
+	if(body.is_in_group("Player")):
+		get_tree().reload_current_scene()
