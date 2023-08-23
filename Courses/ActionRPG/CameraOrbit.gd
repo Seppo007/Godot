3ad4a5dd9c1e6c57e683fcb061d44e0d80911d6d
Sebastian Schalow <schalowseb@gmail.com>
2023-08-23 19:44:24 +0200

    Add horizontal and vertical rotation possibility to camera

---
+++ Courses/ActionRPG/CameraOrbit.gd
@@ -0,0 +1,26 @@
+extends Node3D
+
+var lookSensitivity: float = 15.0
+var minLookAngle: float = -20.0
+var maxLookAngle: float = 75.0
+
+var mouseDelta: Vector2 = Vector2()
+
+@onready var player = get_parent()
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouseDelta = event.relative
+		
+func _process(delta):
+	var rotation = Vector3(mouseDelta.y, mouseDelta.x, 0) * delta * lookSensitivity
+	
+	rotation_degrees.x += rotation.x
+	rotation_degrees.x = clamp(rotation_degrees.x, minLookAngle, maxLookAngle)
+	
+	player.rotation_degrees.y -= rotation.y
+	
+	mouseDelta = Vector2()
